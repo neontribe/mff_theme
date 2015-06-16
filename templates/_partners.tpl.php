b5a47a7b3621ffa982b8744c3b078ec95f14a4df
katjam <katja@neontribe.co.uk>
2015-06-16 10:24:48 +0100

    BAN-34 Add more partner logos

--- templates/_partners.tpl.php
+++ templates/_partners.tpl.php
@@ -2,15 +2,15 @@
             <h2><?=t('Partners in the campaign')?></h2>
             <table>
               <tr>
-                <td><a href="http://www.fairtrade.cz/"><img src="<?=$image_path?>/partner/fair.gif" alt="" /></a></td>
-                <td><a href="http://www.peuples-solidaires.org/"><img src="<?=$image_path?>/partner/peuples.gif" alt="" /></a></td>
+                <td><a href="http://www.ekumakad.cz/"><img src="<?=$image_path?>/partner/EA_logo.png" alt="" /></a></td>
+                <td><a href="http://www.peuples-solidaires.org/"><img src="<?=$image_path?>/partner/peuples.jpg" alt="" /></a></td>
                 <td><a href="http://www.banafair.de/"><img src="<?=$image_path?>/partner/banafair.gif" width="100px" alt="" /></a></td>
                 <td><a href="http://www.bananalink.org.uk/"><img src="<?=$image_path?>/partner/bananalink.gif" width="120px" alt="" /></a></td>
               </tr>
               <tr>
                 <td><a href="http://www.tudatosvasarlo.hu/"><img src="<?=$image_path?>/partner/TVE_logo.png" alt="" /></a></td>
                 <td><a href="http://www.oxfam.de"><img src="<?=$image_path?>/partner/oxfam_de_logo.jpg" alt="" /></a></td>
-                <td><a href="http://www.suedwind-agentur.at/"><img src="<?=$image_path?>/partner/sw_logo.jpg" alt="" /></a></td>
+                <td><a href="http://www.suedwind-agentur.at/"><img src="<?=$image_path?>/partner/sw_logo.png" alt="" /></a></td>
                 <td><a href="http://www.maibine.eu/"><img src="<?=$image_path?>/partner/maibine_logo.png" alt="" /></a></td>
               </tr>
               <tr>
@@ -21,8 +21,8 @@
               </tr>
               <tr>
                 <td><a href="http://www.l-arka.org/about/kkg/"><img src="<?=$image_path?>/partner/kkg_logo.png" alt="" /></a></td>
-                <td></td>
-                <td></td>
+                <td><a href="http://www.fairtrade-advocacy.org/"><img src="<?=$image_path?>/partner/FTAO_logo.png" alt="" /></a></td>
+                <td><a href="http://www.zalabriviba.lv/greenliberty/"><img src="<?=$image_path?>/partner/ZB_logo.png" alt="" /></a></td>
                 <td></td>
               </tr>
             </table>
@@ -32,8 +32,8 @@ Xhttp://ekonsument.pl/
 Xhttp://www.finep.org/
 Xhttp://www.gvc-italia.org
 Xhttp://www.imvf.org
-http://www.l-arka.org/about/kkg
+Xhttp://www.l-arka.org/about/kkg
 Xhttp://www.maibine.eu
 Xhttp://www.suedwind-agentur.at
 Xhttp://www.oxfam.de/
-Xhttp://tudatosvasarlo.hu/ --->
+Xhttp://tudatosvasarlo.hu/Xhttp://www.fairtrade-advocacy.org/Xhttp://www.zalabriviba.lv/greenliberty/ --->
