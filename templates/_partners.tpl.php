35f960b7321b1f0a08da45b017f520853293f59c
katjam <katja@neontribe.co.uk>
2015-06-19 12:36:17 +0100

    Add Winfa and Sintrainagro BAN-48

--- templates/_partners.tpl.php
+++ templates/_partners.tpl.php
@@ -2,44 +2,34 @@
             <h2><?=t('Partners in the campaign')?></h2>
             <table>
               <tr>
-                <td><a href="http://www.ekumakad.cz/"><img src="<?=$image_path?>/partner/EA_logo.png" alt="" /></a></td>
-                <td><a href="http://www.peuples-solidaires.org/"><img src="<?=$image_path?>/partner/peuples.jpg" alt="" /></a></td>
-                <td><a href="http://www.banafair.de/"><img src="<?=$image_path?>/partner/banafair.gif" width="100px" alt="" /></a></td>
-                <td><a href="http://www.bananalink.org.uk/"><img src="<?=$image_path?>/partner/bananalink.gif" width="120px" alt="" /></a></td>
+                <td><a href="http://www.ekumakad.cz/"><img src="<?=$image_path?>/partner/EA_logo.png" alt="Ekumakad" target="_blank" /></a></td>
+                <td><a href="http://www.peuples-solidaires.org/"><img src="<?=$image_path?>/partner/peuples.jpg" alt="Peuples Solidaires" target="_blank" /></a></td>
+                <td><a href="http://www.banafair.de/"><img src="<?=$image_path?>/partner/banafair.gif" width="100px" alt="Banafair" target="_blank" /></a></td>
+                <td><a href="http://www.bananalink.org.uk/"><img src="<?=$image_path?>/partner/bananalink.gif" width="120px" alt="Banana Link" target="_blank" /></a></td>
               </tr>
               <tr>
-                <td><a href="http://www.tudatosvasarlo.hu/"><img src="<?=$image_path?>/partner/TVE_logo.png" alt="" /></a></td>
-                <td><a href="http://www.oxfam.de"><img src="<?=$image_path?>/partner/oxfam_de_logo.jpg" alt="" /></a></td>
-                <td><a href="http://www.suedwind-agentur.at/"><img src="<?=$image_path?>/partner/sw_logo.png" alt="" /></a></td>
-                <td><a href="http://www.maibine.eu/"><img src="<?=$image_path?>/partner/maibine_logo.png" alt="" /></a></td>
+                <td><a href="http://www.tudatosvasarlo.hu/"><img src="<?=$image_path?>/partner/TVE_logo.png" alt="TVE" target="_blank" /></a></td>
+                <td><a href="http://www.oxfam.de"><img src="<?=$image_path?>/partner/oxfam_de_logo.jpg" alt="Oxfam DE" target="_blank" /></a></td>
+                <td><a href="http://www.suedwind-agentur.at/"><img src="<?=$image_path?>/partner/sw_logo.png" alt="Suedwind" target="_blank" /></a></td>
+                <td><a href="http://www.maibine.eu/"><img src="<?=$image_path?>/partner/maibine_logo.png" alt="Maibine" target="_blank" /></a></td>
               </tr>
               <tr>
-                <td><a href="http://www.ekonsument.pl/"><img src="<?=$image_path?>/partner/KO_logo.jpg" alt="" /></a></td>
-                <td><a href="http://www.finep.org"><img src="<?=$image_path?>/partner/fine_p_logo.jpg" alt="" /></a></td>
-                <td><a href="http://www.gvc-italia.org/"><img src="<?=$image_path?>/partner/gvc_italia_logo.png" width="" alt="" /></a></td>
-                <td><a href="http://www.imvf.org/"><img src="<?=$image_path?>/partner/imvf_logo.png" alt="" /></a></td>
+                <td><a href="http://www.ekonsument.pl/"><img src="<?=$image_path?>/partner/KO_logo.jpg" alt="Ekonsument" target="_blank" /></a></td>
+                <td><a href="http://www.finep.org"><img src="<?=$image_path?>/partner/fine_p_logo.jpg" alt="Finep" target="_blank" /></a></td>
+                <td><a href="http://www.gvc-italia.org/"><img src="<?=$image_path?>/partner/gvc_italia_logo.png" alt="GVC Italia" target="_blank" /></a></td>
+                <td><a href="http://www.imvf.org/"><img src="<?=$image_path?>/partner/imvf_logo.png" alt="IMVF" target="_blank" /></a></td>
               </tr>
               <tr>
-                <td><a href="http://www.l-arka.org/about/kkg/"><img src="<?=$image_path?>/partner/kkg_logo.png" alt="" /></a></td>
-                <td><a href="http://www.fairtrade-advocacy.org/"><img src="<?=$image_path?>/partner/FTAO_logo.png" alt="" /></a></td>
-                <td><a href="http://www.zalabriviba.lv/greenliberty/"><img src="<?=$image_path?>/partner/ZB_logo.png" alt="" /></a></td>
+                <td><a href="http://www.l-arka.org/about/kkg/"><img src="<?=$image_path?>/partner/kkg_logo.png" alt="KKG" target="_blank" /></a></td>
+                <td><a href="http://www.fairtrade-advocacy.org/"><img src="<?=$image_path?>/partner/FTAO_logo.png" alt="Fairtrade Advocacy" target="_blank" /></a></td>
+                <td><a href="http://www.zalabriviba.lv/greenliberty/"><img src="<?=$image_path?>/partner/ZB_logo.png" alt="Zala Briviba" target="_blank" /></a></td>
                 <td><a href="http://www.urocal.org"><img src="<?=$image_path?>/partner/urocal.png" alt="Urocal" target="_blank" /></a></td>
               </tr>
               <tr>
-                <td><img src="<?=$image_path?>/partner/FAWU_logo.jpg" /></td>
-                <td></td>
-                <td></td>
+                <td><img src="<?=$image_path?>/partner/FAWU_logo.jpg" alt="Fako Agricultural Workers Union" /></td>
+                <td><img src="<?=$image_path?>/partner/SINTRAINAGRO.jpg" alt="Sintrainagro" /></td>
+                <td><img src="<?=$image_path?>/partner/Winfa_logo.png" alt="Windward Islands Farmers Association" /></td>
                 <td></td>
               </tr>
             </table>
           </div>
-<!---
-Xhttp://ekonsument.pl/
-Xhttp://www.finep.org/
-Xhttp://www.gvc-italia.org
-Xhttp://www.imvf.org
-Xhttp://www.l-arka.org/about/kkg
-Xhttp://www.maibine.eu
-Xhttp://www.suedwind-agentur.at
-Xhttp://www.oxfam.de/
-Xhttp://tudatosvasarlo.hu/Xhttp://www.fairtrade-advocacy.org/Xhttp://www.zalabriviba.lv/greenliberty/ --->
